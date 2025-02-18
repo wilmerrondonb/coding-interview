544514289d7644884410e0634090b48911f96336
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2025-02-17 23:16:39 -0400

    Anagram Strings
    - Two strings are called anagrams if they contain all the same characters in the same frequencies.

--- src/main/java/org/exercise/Main.java
+++ src/main/java/org/exercise/Main.java
@@ -5,7 +5,12 @@ import org.exercise.palindromeword.PalindromeWordTwoPointers;
 public class Main {
 
     public static void main(String[] args) {
+        // Palindrome Words
         PalindromeWordTwoPointers palindromeWordTwoPointers = new PalindromeWordTwoPointers();
         palindromeWordTwoPointers.checkWord();
+
+        // Anagram Strings
+        AnagramString anagramString = new AnagramString();
+        anagramString.checkString();
     }
 }
