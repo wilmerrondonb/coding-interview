f07be86ac58169fa5bdd25b698f6f67a91f726a0
wilmerrondonb <47371083+wilmerrondonb@users.noreply.github.com>
2025-02-20 13:40:38 -0400

    Factorial
    * Factorial
    
    - It is the product of all positive integers less than or equal to the given number.
    
    ---------
    
    Co-authored-by: Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>

---
+++ src/main/java/org/exercise/FactorialIterativeLoop.java
@@ -0,0 +1,27 @@
+package org.exercise;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FactorialIterativeLoop {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    protected void calculateFactorial() {
+        int factorialNumber = 5;
+        int factorialResult = getFactorial(factorialNumber);
+        logger.log(Level.INFO, () -> factorialNumber + "! = " + factorialResult);
+    }
+
+    private int getFactorial(int factorialNumber) {
+        if (factorialNumber == 0 || factorialNumber == 1) {
+            return 1;
+        }
+
+        int factorialResult = factorialNumber;
+        for (int i = factorialNumber - 1; i >= 1; i--) {
+            factorialResult = factorialResult * i;
+        }
+        return factorialResult;
+    }
+}
