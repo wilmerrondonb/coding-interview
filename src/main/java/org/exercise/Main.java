202a321cc216ca05a7eb12b2ff4ee68a000d727c
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2025-02-20 13:31:34 -0400

    Factorial
    - It is the product of all positive integers less than or equal to the given number.

--- src/main/java/org/exercise/Main.java
+++ src/main/java/org/exercise/Main.java
@@ -16,5 +16,9 @@ public class Main {
         // Fibonacci Sequence
         FibonacciDynamicProgramming fibonacciDynamicProgramming = new FibonacciDynamicProgramming();
         fibonacciDynamicProgramming.calculateFibonacci();
+
+        // Factorial
+        Factorial factorial = new Factorial();
+        factorial.calculateFactorial();
     }
 }
